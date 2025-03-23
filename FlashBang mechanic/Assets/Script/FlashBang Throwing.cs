8961fc14f77d6e10bf2ec7cd9a83b4785ea69321
KhoaTran3006 <dangkhoamt302@gmail.com>
2025-03-24 02:29:49 +0700

    DevLog 24/03
    Projection drawn but still bug

--- FlashBang mechanic/Assets/Script/FlashBang Throwing.cs
+++ FlashBang mechanic/Assets/Script/FlashBang Throwing.cs
@@ -13,38 +13,60 @@ public class FlashBangThrowing : MonoBehaviour
     public GameObject flashbangPos;
     public Transform fpsCam, player;
     public Animator animator;
+    public Camera cam;
 
+    [Header("Display Control")]
+    [SerializeField]
+    [Range(10f, 100f)]
+    private int LinePoints = 25;
+    [SerializeField]
+    [Range(0.01f, 0.25f)]
+    private float TimeBetweenPoints = 0.1f;
 
-
+    [SerializeField]
+    private LineRenderer lineRenderer;
     [SerializeField]
     private bool isAiming = false;
     [SerializeField]
     private bool isEquiped = false;
+    
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         flashbangPos.SetActive(false);
+        //cam = Camera.main;
 
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.E)) //press E to toggle equip/unequip
         {
             isEquiped = !isEquiped;
         }
-        if (Input.GetMouseButtonDown(0) && isEquiped) // hold down right mouse button to aim
+        if (Input.GetMouseButton(0) && isEquiped) // hold down right mouse button to aim
         {
             isAiming = true;
+            DrawProjection(); //draw projection when aiming
         }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+        
 
         if (Input.GetMouseButtonUp(0) && isAiming && isEquiped) //release right mouse button to throw
         {
             ThrowBlind();
             isAiming = false;
             isEquiped = false;
+            
         }
 
         //Equip Flashbang on Hand
@@ -72,6 +94,28 @@ public class FlashBangThrowing : MonoBehaviour
         if (rb != null)
         {
             rb.AddForce(throwPoint.forward * throwingForce, ForceMode.VelocityChange);
+        }
+    }
+
+    private void DrawProjection()
+    {
+        float mass = flashbangPrefab.GetComponent<Rigidbody>().mass; // get the flash grenade mass
+        //we will implement kinematic equation d = V0*t + 1/2*a*t^2
+        Debug.Log(mass);
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
+        Vector3 startPosition = throwPoint.position;
+        Vector3 startVelocity = throwingForce * cam.transform.forward / mass;
+        
+        int i = 0;
+        for (float time = 0f; time < LinePoints; time += TimeBetweenPoints)
+        {
+            i++;
+            Vector3 point = startPosition + time * startVelocity;
+            //now implement the equation
+            point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time); //new point.y = startPosition.y, V0 =startVelocity.y, t = time, a = Physics.gravity, t^2 = time *time
+
+            lineRenderer.SetPosition(i, point);
         }
     }
 }
